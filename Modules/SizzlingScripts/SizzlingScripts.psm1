b9ac05b02d33853ea184e6d759a6b2ea1204ca62
ethorneloe <eric.thorneloe@daf.qld.gov.au>
2024-05-03 11:30:45 +1000

    Add module files

---
+++ Modules/SizzlingScripts/SizzlingScripts.psm1
@@ -0,0 +1,19 @@
+# Import Dependencies - Make sure these are also added to the psd1 file
+
+# Import-Module -Name 'DependencyModule1' -MinimumVersion '2.0.0'
+# Import-Module -Name 'DependencyModule2' -MinimumVersion '3.1.0'
+
+# Load private functions
+$privateFunctions = Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Functions/Private') -Filter *.ps1 -Recurse
+foreach ($privateFunction in $privateFunctions) {
+    . $privateFunction.FullName
+}
+
+# Load public functions
+$publicFunctions = Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Functions/Public') -Filter *.ps1 -Recurse
+foreach ($publicFunction in $publicFunctions) {
+    . $publicFunction.FullName
+}
+
+# Export public functions
+Export-ModuleMember -Function $publicFunctions.Basename
